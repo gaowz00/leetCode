dad027d54f6979edc04fdb35e83bc636af8115ff
swoloka <wenzegao@gmail.com>
2021-04-03 13:58:00 +0800

    1110提交  递归

---
+++ 1110/1110.cpp
@@ -0,0 +1,57 @@
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
+    }
+};
+
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete)
+    {
+        vector<TreeNode*> forest;
+        unordered_set<int> dict(to_delete.begin(), to_delete.end());
+        root = helper(root, dict, forest);
+        if (root)
+            forest.push_back(root);
+        return forest;
+    }
+
+    TreeNode* helper(TreeNode* root, unordered_set<int>& dict, vector<TreeNode*>& forest)
+    {
+        if (!root)
+            return root;
+        root->left = helper(root->left, dict, forest);
+        root->right = helper(root->right, dict, forest);
+        if (dict.count(root->val)) {
+            if (root->left)
+                forest.push_back(root->left);
+            if (root->right)
+                forest.push_back(root->right);
+            root = nullptr;
+        }
+        return root;
+    }
+};
